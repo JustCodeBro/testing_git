59c2e648bed673e01385074c8f57ad97a49ffffb
JustCodeBro <harrycoding16@gmail.com>
2025-12-25 19:06:21 +0700

    Adding numpy code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
 def main():
     print("Version 13")
     print("Restoring and making some changes")
+    rng = np.random.dafault_rng(seed=67)
+    arr = rng.uniform(low=0, high=11, size=(10, 3))
 
 
 if __name__ == "__main__":
